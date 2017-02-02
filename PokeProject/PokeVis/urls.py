5a472332646d789f07cbca98661d9e7bbc444ef0
kencar17 <carmichael@kenneths-MacBook-Pro.local>
2017-02-02 09:07:28 -0700

    Add html pages for everyone and poke images

--- PokeProject/PokeVis/urls.py
+++ PokeProject/PokeVis/urls.py
@@ -3,7 +3,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'ken', views.kenView.as_view(), name='ken'),
-
+    url(r'ken', views.KenView.as_view(), name='ken'),
+    url(r'jordan', views.JordanView.as_view(), name='jordan'),
+    url(r'liam', views.LiamView.as_view(), name='liam'),
+    url(r'graham', views.GrahamView.as_view(), name='graham'),
 ]
 
--- PokeProject/PokeVis/views.py
+++ PokeProject/PokeVis/views.py
@@ -4,5 +4,17 @@ from django.shortcuts import render
 from django.views import generic
 
 
-class kenView(generic.TemplateView):
+class KenView(generic.TemplateView):
     template_name = 'ken.html'
+
+
+class JordanView(generic.TemplateView):
+    template_name = 'jordan.html'
+
+
+class LiamView(generic.TemplateView):
+        template_name = 'liam.html'
+
+
+class GrahamView(generic.TemplateView):
+    template_name = 'graham.html'
