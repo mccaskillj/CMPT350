26e455f65621475d1f24d7e30ffa1fb51b946c35
carmichaelk3@mymacewan.ca <carmichaelk3@mymacewan.ca>
2017-02-17 22:44:43 -0700

    Add base template and search html

--- PokeProject/PokeVis/urls.py
+++ PokeProject/PokeVis/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'jordan', views.JordanView.as_view(), name='jordan'),
     url(r'liam', views.LiamView.as_view(), name='liam'),
     url(r'graham', views.GrahamView.as_view(), name='graham'),
+    url(r'search', views.SearchView, name='search'),
 ]
 
