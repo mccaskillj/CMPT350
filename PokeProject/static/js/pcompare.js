a10938555aa5a4ca4476a011e6faf430b994fe72
carmichaelk3@mymacewan.ca <carmichaelk3@mymacewan.ca>
2017-03-08 11:40:38 -0700

    Add double bar graphs for different stats

--- PokeProject/static/js/pcompare.js
+++ PokeProject/static/js/pcompare.js
@@ -13,10 +13,12 @@ function showPokemon() {
             data: {"name": selectPoke},
             dataType: 'JSON',                //data format
             success: function(pokemons) {
-                var dict = [];
-                var unwanted = ['color', 'name', 'id', 'body_style', 'is_legendary', 'catch_rate',
-                                'generation', 'egg_group_1', 'egg_group_2', 'type_2', 'type_1', 'height', 'width', 'total'];
-
+                var unwantedFirst = ['color', 'name', 'id', 'body_style', 'is_legendary', 'catch_rate',
+                                'generation', 'egg_group_1', 'egg_group_2', 'type_2', 'type_1', 'height', 'width', 'total',
+                                'phys_sweeper', 'sp_sweeper', 'wall', 'phys_tank', 'sp_tank'];
+                var unwantedSecond = ['color', 'name', 'id', 'body_style', 'is_legendary', 'catch_rate',
+                                'generation', 'egg_group_1', 'egg_group_2', 'type_2', 'type_1', 'height', 'width', 'total',
+                                'hp', 'attack', 'defense', 'sp_attack', 'sp_defense', 'speed'];
                 var id = pokemons[0]['id'];
 
                 $("#frontImg").attr('src', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + id + '.png');
@@ -38,7 +40,7 @@ function showPokemon() {
                 for (var key in pokemons[0]) {
                     var value = pokemons[0][key];
                     // Use `key` and `value`
-                    if (unwanted.includes(key) != true) {
+                    if (unwantedFirst.includes(key) != true) {
                         dict.push({
                             stat: key,
                             value: value
@@ -46,70 +48,17 @@ function showPokemon() {
                     }
                 }
 
+                var data1 = seperateValues(pokemons, unwantedFirst);
+                var data2 = seperateValues(pokemons, unwantedSecond);
+                seperateValues(pokemons, unwantedSecond);
+
                 $("#singleChart  svg").remove();
-                // d3 stuff
-                var width = 930, height = 480;
-                var margin = {top: 20, right: 20, bottom: 30, left: 40};
+                createDefaultGraphs("#singleChart", data1);
+                $("#singleChart2  svg").remove();
+                createDefaultGraphs("#singleChart2", data2);
 
-                //x and y Scales
-                var xScale = d3.scale.ordinal()
-                    .rangeRoundBands([0, width], .1);
-
-                var yScale = d3.scale.linear()
-                    .range([height, 0]);
-
-                xScale.domain(dict.map(function(d) { return d.stat; }));
-                yScale.domain([0, d3.max(dict, function(d) { return d.value; })]);
-
-                //x and y Axes
-                var xAxis = d3.svg.axis()
-                    .scale(xScale)
-                    .orient("bottom");
-
-                var yAxis = d3.svg.axis()
-                    .scale(yScale)
-                    .orient("left")
-                    .ticks(10, "%");
-
-                //create svg container
-                var svg = d3.select("#singleChart")
-                    .append("svg")
-                    .attr("width", width + margin.left + margin.right)
-                    .attr("height", height + margin.top + margin.bottom)
-                    .append("g")
-                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-                //create bars
-                svg.selectAll(".bar")
-                    .data(dict)
-                    .enter()
-                    .append("rect")
-                    .attr("class", "bar")
-                    .attr("fill", "#90caf9")
-                    .attr("x", function(d) { return xScale(d.stat); })
-                    .attr("width", xScale.rangeBand())
-                    .attr("y", function(d) { return yScale(d.value); })
-                    .attr("height", function(d) { return height - yScale(d.value); });
-
-                //drawing the x axis on svg
-                svg.append("g")
-                    .attr("class", "x axis")
-                    .attr("transform", "translate(0," + height + ")")
-                    .style("font-size","13px")
-                    .call(xAxis);
-
-                //drawing the y axis on svg
-                svg.append("g")
-                    .attr("class", "y axis")
-                    .call(yAxis)
-                    .append("text")
-                    .attr("transform", "rotate(-90)")
-                    .attr("y", 6)
-                    .attr("dy", ".71em")
-                    .style("font-size","13px")
-                    .style("text-anchor", "end")
-
-
+                document.getElementById("checkNormal").checked = true;
+                document.getElementById("checkBar").checked = true;
             },
             failure: function(pokemons) {
                 alert('Got an error dude');
@@ -117,6 +66,21 @@ function showPokemon() {
             });
         }
 
+}
+
+function seperateValues(pokemons, unwanted){
+    var dict = [];
+    for (var key in pokemons[0]) {
+        var value = pokemons[0][key];
+        // Use `key` and `value`
+        if (unwanted.includes(key) != true) {
+            dict.push({
+                stat: key,
+                value: value
+            });
+        }
+    }
+    return dict
 }
 
 function addType(type){
@@ -200,10 +164,10 @@ function openCity(evt, cityName) {
 function addB(){
     var pokeName = document.getElementById("addPoke").value;
     var elem = document.getElementById('pokeBadges');
-    var amount = document.getElementsByClassName('label-info');
+    var amount = document.getElementsByClassName('label-success');
 
-    if (amount.length < 14) {
-        elem.innerHTML += '<span class="label label-info">' + pokeName + '<a name="' + pokeName + '" class="remove" onclick="remove(this)"> <span class="glyphicon glyphicon-remove-circle" style="color: white"></span></a></span> ';
+    if (amount.length < 12) {
+        elem.innerHTML += '<span class="label label-success">' + pokeName + '<a name="' + pokeName + '" class="remove" onclick="remove(this)"> <span class="glyphicon glyphicon-remove-circle" style="color: white"></span></a></span> ';
     }
 }
 
@@ -216,6 +180,194 @@ function remove(elm){
 }
 
 // Default states
+// Multi
+var dict = [];
+
+var width = 880, height = 680;
+var margin = {top: 20, right: 20, bottom: 30, left: 80};
+
+//x and y Scales
+var xScale = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1);
+
+var yScale = d3.scale.linear()
+    .range([height, 0]);
+
+xScale.domain(dict.map(function(d) { return d.stat; }));
+yScale.domain([0, d3.max(dict, function(d) { return d.value; })]);
+
+//x and y Axes
+var xAxis = d3.svg.axis()
+    .scale(xScale)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(yScale)
+    .orient("left")
+    .ticks(10, "%");
+
+//create svg container
+var svg = d3.select("#multiChart")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+//create bars
+svg.selectAll(".bar")
+    .data(dict)
+    .enter()
+    .append("rect")
+    .attr("class", "bar")
+    .attr("fill", "#90caf9")
+    .attr("x", function(d) { return xScale(d.stat); })
+    .attr("width", xScale.rangeBand())
+    .attr("y", function(d) { return yScale(d.value); })
+    .attr("height", function(d) { return height - yScale(d.value); });
+
+//drawing the x axis on svg
+svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .style("font-size","13px")
+    .call(xAxis);
+
+//drawing the y axis on svg
+svg.append("g")
+    .attr("class", "y axis")
+    .call(yAxis)
+    .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 6)
+    .attr("dy", ".71em")
+    .style("font-size","13px")
+    .style("text-anchor", "end");
 
 
+$( function() {
+    var pokemonNames = [];
 
+    $.ajax({
+        type: "GET",
+        url: 'ajax/get_pokemon/', //the script to call to get data
+        dataType: 'JSON',                //data format
+        success: function(pokemons) {
+            for(var i = 0; i < pokemons.length; i++) {
+                pokemonNames.push(pokemons[i]['name'])
+            }
+
+        },
+        failure: function(pokemons) {
+            alert('Got an error dude');
+        }
+    });
+
+    $( "#tags" ).autocomplete({
+      source: pokemonNames
+    });
+
+    $( "#poke1" ).autocomplete({
+      source: pokemonNames
+    });
+
+    $( "#poke2" ).autocomplete({
+      source: pokemonNames
+    });
+
+    $( "#addPoke" ).autocomplete({
+      source: pokemonNames
+    });
+} );
+
+
+$('#shinny').on("click",function(){
+    var id = document.getElementById('pokedex').innerHTML;
+    $("#frontImg").attr('src', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/' + id + '.png');
+    $("#backImg").attr('src', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/' + id + '.png');
+});
+
+$('#normal').on("click",function(){
+    var id = document.getElementById('pokedex').innerHTML;
+    $("#frontImg").attr('src', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + id + '.png');
+    $("#backImg").attr('src', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/' + id + '.png');
+});
+
+// Add listener for enter key press
+$("#tags").keyup(function(event){
+    if(event.keyCode == 13){
+        $("#sb").click();
+    }
+});
+
+createDefaultGraphs("#singleChart", []);
+createDefaultGraphs("#singleChart2", []);
+
+function createDefaultGraphs(id, data){
+    // Have default empty table for compare screen
+    var dict = [];
+
+    var width = 350, height = 480;
+    var margin = {top: 20, right: 20, bottom: 60, left: 40};
+
+    //x and y Scales
+    var xScale = d3.scale.ordinal()
+        .rangeRoundBands([0, width], .1);
+
+    var yScale = d3.scale.linear()
+        .range([height, 0]);
+
+    xScale.domain(data.map(function(d) { return d.stat; }));
+    yScale.domain([0, d3.max(data, function(d) { return d.value; })]);
+
+    //x and y Axes
+    var xAxis = d3.svg.axis()
+        .scale(xScale)
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(yScale)
+        .orient("left")
+        .ticks(10, "%");
+
+    //create svg container
+    var svg = d3.select(id)
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    //create bars
+    svg.selectAll(".bar")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("class", "bar")
+        .attr("fill", "#90caf9")
+        .attr("x", function(d) { return xScale(d.stat); })
+        .attr("width", xScale.rangeBand())
+        .attr("y", function(d) { return yScale(d.value); })
+        .attr("height", function(d) { return height - yScale(d.value); });
+
+    //drawing the x axis on svg
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .style("font-size","13px")
+        .call(xAxis)
+        .selectAll("text")
+        .attr("transform", "rotate(-30)")
+        .style("text-anchor", "end");
+
+    //drawing the y axis on svg
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("font-size","13px")
+        .style("text-anchor", "end");
+}
